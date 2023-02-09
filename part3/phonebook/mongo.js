c339a21b50b124e5e608b8c7cab67f75c5fb65fb
Solaxi <esko.pylsy@gmail.com>
2023-02-09 18:03:46 +0200

    Fixed lint errors

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -9,11 +9,11 @@
 
 */
 
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log("Please give pw as an argument");
-    process.exit(1)
+  console.log('Please give pw as an argument')
+  process.exit(1)
 }
 const pw = process.argv[2]
 const url = `mongodb+srv://fullstack:${pw}@cluster0.w1su2nx.mongodb.net/phonebookApp?retryWrites=true&w=majority`
@@ -22,8 +22,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -32,19 +32,19 @@ const Person = mongoose.model('Person', personSchema)
 const name = process.argv[3]
 const number = process.argv[4]
 if (name && number) {
-    const person = new Person({ name, number })
-    person.save().then(result => {
-        console.log(`Added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()        
-    })
+  const person = new Person({ name, number })
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 
 /* Else list all*/
 } else {
-    Person.find({}).then(result => {
-        console.log(`Phonebook (${result.length} entries):`)
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log(`Phonebook (${result.length} entries):`)
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 }
